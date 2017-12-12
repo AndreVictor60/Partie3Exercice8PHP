4820ad3915a570df6338b27a68badd525d7bc090
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:12:06 +0100

    fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 8</title>
+  </head>
+  <body>
+    <?php
+      for($pas = 200; $pas >= 0;$pas-=12){
+        echo 'Enfin !!!<br />';
+      }
+     ?>
+  </body>
+</html>
